f3fbefa9b930329da3bf8f9ae9b71ba688688fc8
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-05 09:28:29 +0530

    Setup tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use eyre::eyre::Result;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing_utils::{format::SourceFormatter, writer::RotatingFileWriter};
+
+fn main() -> Result<()> {
+    let (non_blocking, _guard) = tracing_appender::non_blocking(RotatingFileWriter::new(
+        3,
+        PathBuf::from(".").canonicalize()?,
+        "workshop-uploader.log",
+    )?);
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .event_format(SourceFormatter)
+                .with_writer(non_blocking),
+        )
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::DEBUG.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    Ok(())
 }
